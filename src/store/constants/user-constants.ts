6d54b13353bf6bdb51905fa8fca6c2a9b919c35b
thoffart <thiagohoffart321920@gmail.com>
2020-02-06 16:07:36 -0300

    feat(redux): adding user-constants

---
+++ src/store/constants/user-constants.ts
@@ -0,0 +1,7 @@
+export const LOAD_USERS = '[Users] Load Users';
+export const LOAD_USERS_FAIL = '[Users] Load Users Fail';
+export const LOAD_USERS_SUCCESS = '[Users] Load Users Success';
+export const LOAD_USER_All_DATA = '[Users] Load User All Data';
+export const LOAD_USER_All_DATA_FAIL = '[Users] Load User All Data Fail';
+export const LOAD_USER_All_DATA_SUCCESS = '[Users] Load User All Data Success';
+export const CHANGE_USERS_FILTERTEXT = '[Users] Change Users filterText';
