43a019fed6ed63708c7436f024fa9008f011bb57
thoffart <thiagohoffart321920@gmail.com>
2020-02-06 16:11:46 -0300

    feat(redux & router): providing the store and the react-router into the index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Startup from './components/startup/startup';
+import HomePage from './pages/home-page/home-page';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import userPage from './pages/home-page/user-page/user-page';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+      <Startup>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/users/:id' component={userPage} />
+          </Switch>
+        </Router>
+      </Startup>
+    </Provider>,
+    document.getElementById("root")
+);
+  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
