afddc30a5f2d6bcd2fd8e187a47e10f7738d7ecf
thoffart <thiagohoffart321920@gmail.com>
2020-02-06 16:07:48 -0300

    feat(redux): adding organization-constants

---
+++ src/store/constants/organization-constants.ts
@@ -0,0 +1,3 @@
+export const LOAD_ORGANIZATION = '[Organization] Load Organization';
+export const LOAD_ORGANIZATION_FAIL = '[Organization] Load Organization Fail';
+export const LOAD_ORGANIZATION_SUCCESS = '[Organization] Load Organization Success';
